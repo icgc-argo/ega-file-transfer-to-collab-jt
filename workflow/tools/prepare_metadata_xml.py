2a1dab0d5cfd3a49a5da7bec88f613a8895b667b
baminou <brice.aminou@gmail.com>
2018-12-18 09:58:32 -0500

    https checked added for preparing metadata xml step

--- workflow/tools/prepare_metadata_xml.py
+++ workflow/tools/prepare_metadata_xml.py
@@ -47,6 +47,9 @@ task_start = int(time.time())
 metadata_container = "quay.io/baminou/dckr_prepare_metadata_xml"
 subprocess.check_output(['docker', 'pull', metadata_container+':1.0'])
 
+if not ega_metadata_repo.startswith('https://'):
+    ega_metadata_repo = 'https://'+ega_metadata_repo
+
 try:
     subprocess.check_output(['docker','run',
                              '-v', out_dir + ':/app',
